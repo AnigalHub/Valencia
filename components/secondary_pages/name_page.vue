0d943510378d4b8860df46d3e7151130d95ba01e
AnigalHub <anigal1997@gmail.com>
2021-01-18 23:25:53 +0300

    стили и компоненты для второстепенных страниц (всех или несколько - кроме главной)

---
+++ components/secondary_pages/name_page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="place_h1">
+    <b-container>
+      <h1>{{name_page}}</h1>
+    </b-container>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "name_page",
+      props:{
+        name_page: String
+        }
+    }
+</script>
+
+<style scoped>
+  h1{
+    padding-left: 15px;
+    font-family: 'Vollkorn', serif;
+    font-weight: 600;
+  }
+  .place_h1{
+    background: #e8edf5;
+    padding: 5px 0;
+    margin-bottom: 2%;
+  }
+</style>
