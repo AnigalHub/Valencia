bf532834d6ce660c873ed0976b5fc2408164ed5a
AnigalHub <anigal1997@gmail.com>
2021-01-18 23:27:22 +0300

    переназван класс для названия компании в меню (была ошибка - одинаковое название с классом на страницах)

--- components/navbar.vue
+++ components/navbar.vue
@@ -6,7 +6,7 @@
              width="64" height="64"
              viewBox="0 0 172 172"
              style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g><path d="M86,151.84375c33.325,-19.75313 53.75,-54.95938 53.75,-94.19687v-20.29063l-53.75,-19.8875l-53.75,19.8875v20.29062c0,39.2375 20.425,74.44375 53.75,94.19688" fill="none"></path><path d="M86,155.875c-0.67187,0 -1.47813,-0.13438 -2.01562,-0.5375c-34.9375,-20.69375 -55.76562,-57.24375 -55.76562,-97.69063v-20.29062c0,-1.74688 1.075,-3.225 2.6875,-3.7625l53.75,-19.8875c0.94062,-0.26875 1.88125,-0.26875 2.82187,0l53.61563,19.8875c1.6125,0.5375 2.6875,2.15 2.6875,3.7625v20.29062c0,40.44688 -20.82812,76.99687 -55.76562,97.69063c-0.5375,0.40312 -1.34375,0.5375 -2.01562,0.5375zM36.28125,40.17813v17.46875c0,36.81875 18.54375,70.00938 49.71875,89.49375c31.175,-19.35 49.71875,-52.675 49.71875,-89.49375v-17.46875l-49.71875,-18.40938z" fill="#ffffff"></path><path d="M86,136.525c-0.80625,0 -1.74688,-0.26875 -2.41875,-0.80625c-21.36562,-15.72187 -34.9375,-38.96875 -38.43125,-65.17187c-0.26875,-2.15 1.20938,-4.16563 3.49375,-4.56875c2.15,-0.26875 4.16562,1.20937 4.56875,3.49375c3.09063,22.97813 14.64688,43.40313 32.7875,57.78125c21.36563,-16.93125 33.59375,-42.19375 33.59375,-69.74062v-6.18125l-33.59375,-12.3625l-36.28125,13.4375c-2.15,0.80625 -4.43437,-0.26875 -5.24063,-2.41875c-0.80625,-2.15 0.26875,-4.43437 2.41875,-5.24063l37.75938,-13.84063c0.94062,-0.26875 1.88125,-0.26875 2.82187,0l37.625,13.975c1.6125,0.5375 2.6875,2.15 2.6875,3.7625v9.00313c0,31.175 -14.24375,59.6625 -39.2375,78.07188c-0.80625,0.5375 -1.74688,0.80625 -2.55312,0.80625z" fill="#3498db"></path><g fill="#3498db"><path d="M86,34.66875v97.825c23.78437,-17.60313 37.625,-44.88125 37.625,-74.84688v-9.00313z"></path></g></g></g></svg>
-        <span class="name">ООО "Валенсия"</span>
+        <span class="name_brand">ООО "Валенсия"</span>
         <p class="chop">частное охранное предприятие</p>
       </b-navbar-brand>
       <a href="" class="telephone">+7(999)999-99-99</a>
@@ -21,12 +21,12 @@
           <b-nav-item to="/company">О нас</b-nav-item>
           <b-nav-item>
             <b-dropdown text="Услуги">
-              <b-dropdown-item to="/">Охрана организаций</b-dropdown-item>
+              <b-dropdown-item to="/services/protection_of_organizations">Охрана организаций</b-dropdown-item>
               <b-dropdown-item to="/services/object_protection">Охрана объектов</b-dropdown-item>
               <b-dropdown-item to="/services/protection_of_real_estate">Охрана недвижимости</b-dropdown-item>
-              <b-dropdown-item to="/bodyguard">Личная охрана</b-dropdown-item>
-              <b-dropdown-item to="/protection_of_events">Охрана мероприятий </b-dropdown-item>
-              <b-dropdown-item to="/cargo_escort">Сопровождение грузов</b-dropdown-item>
+              <b-dropdown-item to="/services/bodyguard">Личная охрана</b-dropdown-item>
+              <b-dropdown-item to="/services/protection_of_events">Охрана мероприятий </b-dropdown-item>
+              <b-dropdown-item to="/services/cargo_escort">Сопровождение грузов</b-dropdown-item>
             </b-dropdown>
           </b-nav-item>
           <b-nav-item href="/tariffs">Тарифы</b-nav-item>
@@ -67,7 +67,7 @@
     font-size: 1.25rem;
     background: #040610;
     letter-spacing: -1.5px;
-    border: 0.5px solid rgba(75, 105, 119, 0.66) !important;
+    //border: 0.5px solid rgba(75, 105, 119, 0.66) !important;
   }
   .dropdown-item:hover{
     background: rgb(37, 74, 94) !important;
@@ -99,6 +99,13 @@
   .navbar{
     padding: 0 !important;
     background: #040610;
+  }
+  .name_brand{
+    font-family: 'Viaoda Libre', cursive;
+    font-weight: 800 !important;
+    letter-spacing: -1.5px;
+    color: #ffffff;
+    text-shadow: 0.5px 0.5px 1px rgb(0, 0, 15);
   }
   svg{
     margin-top: 2.5%;
