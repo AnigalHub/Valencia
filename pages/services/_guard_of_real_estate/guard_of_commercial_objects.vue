2d5cadcc4005e0796d924000b489344a38163119
AnigalHub <anigal1997@gmail.com>
2021-01-20 23:20:54 +0300

    скелет страниц разновидностей охран невижимости (коммерческая, частная)

---
+++ pages/services/_guard_of_real_estate/guard_of_commercial_objects.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="guard_of_commercial_objects" class="page">
+    <Name_page :name_page="name_page"/>
+    <b-container>
+      <b-row>
+        <b-col>
+          <img src="../../../static/133.jpg">
+        </b-col>
+        <b-col cols="7">
+          <p class="text">В охране нуждаются здания, сооружения и иные объекты коммерческой деятельности, безопасность таких объектов - самое важное условие их бесперебойной работы.</p>
+          <b-row class="import">
+            <b-col v-for="guard in GuardOfCommercialObjects" :key="guard.index">
+              <div class="li_text" v-for="name in guard.guard">
+                <component :is="name.svg"/>{{name.name}}
+              </div>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row class="im_descr">
+        <b-col cols="8">
+          <h6>Круг обязанностей охранников коммерческих объектов:</h6>
+          <ul class="text">
+            <li v-for="name_duty in DutiesGuardOfCommercialObjects" :key="name_duty.index">{{name_duty.duty}}</li>
+          </ul>
+        </b-col>
+        <b-col>
+         <img src="../../../static/121.jpg" >
+        </b-col>
+      </b-row>
+      <Tarrifs_content/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+    import Name_page from "../../../components/secondary_pages/name_page";
+    import Guard_shieldSVG from "../../../components/secondary_pages/services_svg/guard_shield_svg";
+    import Tarrifs_content from "../../../components/tarrifs/tarrifs_content";
+    export default {
+      name: "guard_of_commercial_objects",
+      components: {Tarrifs_content, Name_page},
+      data() {
+        return {
+          name_page: "Охрана коммерческих объектов",
+          GuardOfCommercialObjects:[
+            {guard: [{name:"Охрана букмекерских контор",svg:Guard_shieldSVG}, {name:"Охрана банков",svg:Guard_shieldSVG}, {name:"Охрана ресторанов и баров",svg:Guard_shieldSVG},{name:"Охрана торговых центров",svg:Guard_shieldSVG}, {name:"Охрана офисов и бизнес-центров",svg:Guard_shieldSVG},]},
+            {guard: [{name:"Охрана салонов красоты",svg:Guard_shieldSVG}, {name:"Охрана гостиниц и отелей",svg:Guard_shieldSVG}, {name:"Охрана ночных клубов",svg:Guard_shieldSVG},{name:"Охрана магазинов и супермаркетов",svg:Guard_shieldSVG},{name:"Охрана санаториев и домов отдыха",svg:Guard_shieldSVG},]},
+          ],
+          DutiesGuardOfCommercialObjects:[
+            {duty:"защита полной сохранности имущества коммерческой недвижимости, жизни и здоровья людей, а также посетителей объекта;",},
+            {duty:"выставление внутренних постов охраны для контроля за входами-выходами;",},
+            {duty:"регулярные обходы мобильными группами по периметру территории;",},
+            {duty:"мониторинг за помещениями коммерческой недвижимости при помощи видеонаблюдения, позволяющий вовремя реагировать на возникающие конфликтные ситуации;",},
+            {duty:"поддержание общественного порядка внутри здания;",},
+            {duty:"заблаговременное выявление в толпе подозрительных личностей;",},
+            {duty:"борьба с корпоративным шпионажем;",},
+            {duty:"проведение антикриминальных, антивандальный, антитеррористических и противопожарных мероприятий;",},
+            {duty:"своевременное реагирование на любые чрезвычайные ситуации;",},
+            {duty:"контроль за персоналом и посетителями с досмотром личных вещей на предмет хищения;",},
+            {duty:"при незаконных действиях на территории коммерческой недвижимости вызов ГБР;",},
+            {duty:"индивидуальные обязанности в зависимости от специфики коммерческого объекта (личная охрана, запрет на алкоголь, пропускной режим, фейс-контроль и т.д.)",},
+          ],
+        }
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+
+  .row:first-child{
+    padding-bottom: 2% !important;
+  }
+  .row:nth-child(2){
+    padding-bottom: 0.05% !important;
+  }
+  .row:nth-child(4){
+    padding-bottom: 1.5%;
+  }
+  .row:nth-child(3), .row:nth-child(5){
+    ul{
+      margin-left: -8%;
+    }
+  }
+  .im_descr .col{
+  padding: 10% 0 0 0 !important;
+}
+  li{
+    padding-bottom: 0.85%;
+  }
+  .col-4{
+    padding: 6% 0 0 0 !important;
+  }
+   .col .col, .col-9 .col, .import .col{
+    padding: 0 5px !important;
+  }
+  .col-7 .col{
+    padding: 0 !important;
+  }
+  .import ul{
+    margin-left: -8%;
+  }
+
+</style>
---
+++ pages/services/_guard_of_real_estate/guard_private_property.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="guard_private_property" class="page">
+    <Name_page :name_page="name_page"/>
+    <b-container>
+      <b-row>
+        <b-col>
+          <img src="../../../static/988.jpg">
+        </b-col>
+        <b-col cols="7">
+          <p class="text">При охране частных объектов мы учитываем все пожелания наших клиентов, создаем индивидуальную программу охраны под каждый уникальный случай и обеспечиваем максимально возможную сохранность жизни и имущества в рамках конкретной задачи.</p>
+              <div class="li_text" v-for="name in GuardPrivateProperty">
+                <component :is="name.svg"/>{{name.name}}
+              </div>
+        </b-col>
+      </b-row>
+      <b-row class="im_descr">
+        <b-col cols="8">
+          <h6>Круг обязанностей охранников частных объектов:</h6>
+              <ul class="text">
+                <li v-for="name_duty in DutiesGuardPrivateProperty" :key="name_duty.index">{{name_duty.duty}}</li>
+              </ul>
+        </b-col>
+        <b-col>
+          <img src="../../../static/677.jpg" >
+        </b-col>
+      </b-row>
+      <Tarrifs_content/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+    import Name_page from "../../../components/secondary_pages/name_page";
+    import Guard_shieldSVG from "../../../components/secondary_pages/services_svg/guard_shield_svg";
+    import Tarrifs_content from "../../../components/tarrifs/tarrifs_content";
+    export default {
+        name: "guard_private_property",
+      components: {Tarrifs_content, Name_page},
+      data(){
+        return{
+          name_page: "Охрана частных объектов",
+          GuardPrivateProperty:[
+            {name:"Охрана коттеджей и коттеджных поселков",svg:Guard_shieldSVG},
+            {name:"Охрана ТСЖ, жилых комплексов и многоквартирных домов",svg:Guard_shieldSVG},
+            {name:"Охрана часных домов и загородных резиденций",svg:Guard_shieldSVG},
+            {name:"Охрана дач и дачных поселков",svg:Guard_shieldSVG},
+          ],
+          DutiesGuardPrivateProperty:[
+            {duty:"вооруженная охрана со спецсредствами;",},
+            {duty:"защита людей, находящихся в частном объекте, и имущества частного объекта;",},
+            {duty:"борьба с грабежом и другими противоправными посягательствами;",},
+            {duty:"обустройство КПП и резервного поста для обеспечения непрерывного контроля частной территории;",},
+            {duty:"применение комбинированных или альтернативных мер безопасности;",},
+            {duty:"быстрое реагирование при возникшей угрозе;",},
+            {duty:"патрулирование частного объекта для осмотра здания и прилегающих участков;",},
+            {duty:"соблюдение правопорядка на вверенном объекте;",},
+            {duty:"контроль прохода и проезда на территорию, а также подтверждение посторонних лиц жильцами частного объекта;",},
+            {duty:"своевременное оповещение владельца частного объекта в случае форс-мажора;",},
+            {duty:"отслеживание работоспособности систем: водоснабжения, отопления и пожаротушения;",},
+            {duty:"оперативное взаимодействие с органами МВД и МЧС при возникновении нештатных ситуаций, угрожающих сохранности имущества или безопасности жизни и здоровья людей;",},
+            {duty:"профилактика терроризма.",},
+          ]
+        }
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+  .row:first-child{
+    padding-bottom: 2% !important;
+  }
+  .row:nth-child(2){
+    padding-bottom: 0.05% !important;
+  }
+  .row:nth-child(4){
+    padding-bottom: 1.5%;
+  }
+  .row:nth-child(3), .row:nth-child(5){
+    ul{
+      margin-left: -8%;
+    }
+  }
+  .im_descr .col{
+    padding: 10% 0 0 0 !important;
+  }
+  li{
+    padding-bottom: 0.85%;
+  }
+  .col-4{
+    padding: 6% 0 0 0 !important;
+  }
+  .col .col, .col-9 .col, .import .col{
+    padding: 0 5px !important;
+  }
+  .col-7 .col{
+    padding: 0 !important;
+  }
+  .import ul{
+    margin-left: -8%;
+  }
+
+</style>
+
