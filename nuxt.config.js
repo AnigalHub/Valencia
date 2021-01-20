7a522f18d44e1c648ef5ed79ce878eb65e7ef3fe
AnigalHub <anigal1997@gmail.com>
2021-01-20 23:26:13 +0300

    добавлен title страниц и favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'valencia',
+    title: ' ЧОП "ООО Валенсия"',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icons.png' }
     ]
   },
 
