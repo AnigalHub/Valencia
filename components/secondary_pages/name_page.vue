32a7e7528ef97c5b31f0f13031a15ad62d54bc34
AnigalHub <anigal1997@gmail.com>
2021-01-24 21:12:07 +0300

    вынесены стили в общий файл

--- components/secondary_pages/name_page.vue
+++ components/secondary_pages/name_page.vue
@@ -16,14 +16,5 @@
 </script>
 
 <style scoped>
-  h1{
-    padding-left: 15px;
-    font-family: 'Vollkorn', serif;
-    font-weight: 600;
-  }
-  .place_h1{
-    background: #bcc6d4;
-    padding: 5px 0;
-    margin-bottom: 2%;
-  }
+
 </style>
