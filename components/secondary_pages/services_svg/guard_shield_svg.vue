76509d8038da43bdbf72bc56e6c42366c83becfb
AnigalHub <anigal1997@gmail.com>
2021-01-24 21:15:32 +0300

    удалены стили, добавлен id

--- components/secondary_pages/services_svg/guard_shield_svg.vue
+++ components/secondary_pages/services_svg/guard_shield_svg.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+  <svg id="guard_shield_svg" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
        width="100" height="100"
        viewBox="0 0 226 226"
        style=" fill:#000000;">
@@ -48,9 +48,5 @@
 </script>
 
 <style scoped>
-  svg{
-    margin-top: -1%;
-    width: 48px;
-    height: 50px;
-  }
+
 </style>
