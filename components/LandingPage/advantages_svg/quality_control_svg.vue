f9613eebe456cd032579a86bf06d81677b59c08d
AnigalHub <anigal1997@gmail.com>
2021-01-26 23:29:48 +0300

    svg для преимуществ на главной страницы

---
+++ components/LandingPage/advantages_svg/quality_control_svg.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 172 172" style=" fill:#000000;">
+    <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="none" stroke-linecap="none" stroke-linejoin="none" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+      <path d="M0,172v-172h172v172z" fill="none" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter"></path>
+      <g id="Layer_1">
+        <path d="M115.5625,77.9375c-15.05,-10.2125 -25.12812,-26.3375 -27.54687,-44.47812l-0.80625,-5.375c-0.80625,-5.9125 -6.18125,-9.94375 -12.09375,-9.1375c-5.9125,0.80625 -9.94375,6.18125 -9.1375,12.09375l0.80625,5.375c0.67188,5.24062 1.88125,10.34687 3.62813,15.31875l4.43437,12.76563h-25.2625c-8.33125,0 -14.64688,7.525 -13.16875,15.85625l8.6,48.375c1.20938,6.45 6.71875,11.01875 13.16875,11.01875h81.56563v-61.8125z" fill="none" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter"></path>
+        <path d="M103.2,66.91875c-8.0625,-9.40625 -13.4375,-20.9625 -15.18438,-33.45937l-0.80625,-5.375c-0.80625,-5.9125 -6.18125,-9.94375 -12.09375,-9.1375c-5.9125,0.80625 -9.94375,6.18125 -9.1375,12.09375l0.80625,5.375c0.67188,5.24062 1.88125,10.34687 3.62813,15.31875l4.43437,12.76563h-25.2625c-8.33125,0 -14.64688,7.525 -13.16875,15.85625l8.6,48.375c1.20938,6.45 6.71875,11.01875 13.16875,11.01875h41.25313" fill="none"
+              stroke="#71c2ff" stroke-width="8.0625" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M115.5625,139.75v-61.8125h24.1875v61.8125z" fill="none"
+              stroke="#ffffff" stroke-width="8.0625" stroke-linecap="round" stroke-linejoin="round"></path>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+    export default {
+        name: "quality_control_svg"
+    }
+</script>
+
+<style scoped>
+
+</style>
