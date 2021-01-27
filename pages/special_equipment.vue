3f19193aaecbc2f5f75350a57f1729d185ce362a
AnigalHub <anigal1997@gmail.com>
2021-01-27 23:18:02 +0300

    изменена часть с описанием и выводом специальных средств

--- pages/special_equipment.vue
+++ pages/special_equipment.vue
@@ -2,27 +2,17 @@
   <div id="special_equipment" class="page">
     <Name_page :name_page="name_page"/>
     <b-container>
-      <b-row>
-        <b-col cols="3">
-          <img src="../static/special_equipment/special.jpg">
-        </b-col>
-        <b-col>
+          <img src="../static/special_equipment/special.jpg" class="first_img">
           <p class="text">Для обеспечения безопасности охраняемого объекта, поддержания правопорядка и нейтрализации правонарушителей в частном охранном предприятии  “ООО Валенсия” имеются специальные средства индивидуальной защиты - особая экипировка и оснащение охранника.</p>
           <p class="text">Личный состав частного охранного предприятия “ООО Валенсия” - охранники 5-го и 6-го разряда (работа со спецсредствами и оружием).</p>
           <p class="text">Исходя из рисков и угроз, для каждого охраняемого объекта подбирается своя экипировка и средства защиты охраны.</p>
-        </b-col>
-      </b-row>
-      <h6>Перечень специальных средств, используемых в частном охранном предприятии “ООО Валенсия”:</h6>
+      <h6 class="block_two">Перечень специальных средств, используемых в частном охранном предприятии “ООО Валенсия”:</h6>
       <div class="flex-container">
-        <b-row class="beautiful_block" v-for="equipment in ListOfSpecialEquipments" :key="equipment.index">
-          <b-col cols="3">
+        <div class="beautiful_block" v-for="equipment in ListOfSpecialEquipments" :key="equipment.index">
             <img :src="equipment.img_src" :alt="equipment.img_alt"/>
-          </b-col>
-          <b-col>
             <p class="text emphasized">{{equipment.name_equipment}}</p>
             <p class="text">{{equipment.description_equipment}}</p>
-          </b-col>
-        </b-row>
+        </div>
       </div>
     </b-container>
   </div>
@@ -47,12 +37,12 @@
               description_equipment:"Отражения вооруженного нападения на охраняемый объект, подача сигнала тревоги (выстрел в воздух).",
             },
             {
-              name_equipment:"Наручники", img_src:"../special_equipment/handcuffs.jpg", img_alt:"наручники",
-              description_equipment:"Сковывание кистей рук правонарушителей для исключения буйного поведения при сопровождении и ковоировании.",
-            },
-            {
               name_equipment:"Металлодетектор", img_src:"../special_equipment/metal_detector.jpg", img_alt:"металлодетектор",
               description_equipment:"Обнаружение запрещенных металлических средств к внесению и выносу его на территорию охраняемого объекта.",
+            },
+            {
+              name_equipment:"Бронежилет", img_src:"../special_equipment/bulletproof_vest.jpg", img_alt:"бронежилет",
+              description_equipment:"Защита от вооруженного нападения: пули определенного калибра, ножа или заточки в область грудной клетки.",
             },
             {
               name_equipment:"Дубинка (резиновая палка)", img_src:"../special_equipment/baton.jpg", img_alt:"дубинка",
@@ -63,8 +53,8 @@
               description_equipment:"Защита от пулевых и осколочных ранений, различными видами оружия и другими поражающими факторами.",
             },
             {
-              name_equipment:"Бронежилет", img_src:"../special_equipment/bulletproof_vest.jpg", img_alt:"бронежилет",
-              description_equipment:"Защита от вооруженного нападения: пули определенного калибра, ножа или заточки в область грудной клетки.",
+              name_equipment:"Наручники", img_src:"../special_equipment/handcuffs.jpg", img_alt:"наручники",
+              description_equipment:"Сковывание кистей рук правонарушителей для исключения буйного поведения при сопровождении и ковоировании.",
             },
             {
               name_equipment:"Электрошокер", img_src:"../special_equipment/stun_gun.jpg", img_alt:"электрошокер",
