59b072cfd86f04617713ace0f0e8619fb5bf996a
AnigalHub <anigal1997@gmail.com>
2021-02-01 23:25:09 +0300

    переписано через flex

--- components/LandingPage/import_numbers.vue
+++ components/LandingPage/import_numbers.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="Import_numbers">
-      <b-row class="number_row">
-        <b-col v-for="(important_number, index) in important_numbers" :key="index">
-          <div class="number">{{important_number.number}}</div>
-          <p class="text">{{important_number. descr_number}}</p>
-        </b-col>
-      </b-row>
+      <b-container>
+        <div class="flex-container">
+          <div v-for="(important_number, index) in important_numbers" :key="index">
+            <div class="number">{{important_number.number}}</div>
+            <p class="text1" v-html="important_number. descr_number"></p>
+          </div>
+        </div>
+      </b-container>
     </div>
 </template>
 
@@ -17,19 +19,19 @@
             important_numbers:[
               {
                 number:15,
-                descr_number: "Лет на рынке безопасности",
-              },
-              {
-                number:50,
-                descr_number: "Благодарностей и рекомендаций",
+                descr_number: "Лет на рынке <br> безопасности",
               },
               {
                 number:564,
                 descr_number: "Профессиональных сотрудников",
               },
               {
+                number:50,
+                descr_number: "Благодарностей и рекомендаций",
+              },
+              {
                 number:74,
-                descr_number: "Постоянных клиентов",
+                descr_number: "Постоянных <br> клиентов",
               },
             ],
           }
@@ -38,32 +40,37 @@
 </script>
 
 <style scoped>
-  #Import_numbers{
-    padding: 2.5% 0;
-  }
+
   .number{
     font-size:5rem;
     text-align: center;
-    font-family: 'Texturina', serif;
-    transform: scale(1.45);
-    font-weight: 500;
+    font-family: 'Vollkorn', serif;
+    font-weight: 400;
+    text-shadow: 2px 2px 2px rgb(15, 37, 56);
+    transform: scale(1.35);
     letter-spacing: -4px;
     margin-top: -5%;
-    text-shadow: 2px 2px 2px rgb(15, 37, 56);
-    color: #dce5ec;
+    color: white;
   }
-  .number_row{
+  .flex-container{
     margin-left: 8% !important;
     margin-right: 8% !important;
   }
-  .text{
-    font-family: 'Viaoda Libre', cursive;
-    font-weight: 700;
+  .flex-container > div {
+    width: 20%;
+    padding: 0.25%;
+    margin: 1.5% 1% 0.5% 1% !important;
+  }
+  .text1{
+    font-family: 'Source Serif Pro', serif;
+    line-height: 1.4rem;
     transform: scale(1.05);
     margin-top: -5%;
     font-size: 1.35rem;
     text-align: center;
-    text-shadow: 1.5px 2px 2px rgb(15, 37, 56);
-    color: #dce5ec;
+    font-weight: 600;
+    letter-spacing: -1px;
+    text-shadow: 0 2.5px 0  rgb(15, 37, 56) !important;
+    color: white;
   }
 </style>
