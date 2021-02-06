121ed13902f7307f3749efe16d92da5758d03f36
AnigalHub <anigal1997@gmail.com>
2021-02-06 04:26:30 +0300

    изменена структура вывода - с помощью flex и без картинок

--- components/secondary_pages/tarrifs_content.vue
+++ components/secondary_pages/tarrifs_content.vue
@@ -3,104 +3,111 @@
     <b-container>
       <div class="flex-container">
         <div>
-          <h6>Эконом</h6>
+          <h3>Эконом</h3>
           <b-table class="text" hover :items="items" :fields="fields">
             <template v-slot:cell(econom)="{item}">
-              <h4>{{item.name}}</h4>
+              <div class="text"><b>{{item.name}}</b></div>
               <span class="text_descr">{{item.descr_name}}</span>
             </template>
             <template v-slot:cell(tt)="{item}">
-              <div> от {{item.price}} ₽/час</div>
+              <div> от {{item.price_hour}} ₽/час</div>
               <div> от {{item.price}} ₽/мес</div>
             </template>
             <template v-slot:cell(ttr)="{item}">
-              <div>от {{item.price_with_weapon}} ₽/час </div>
-              <div> от {{item.price}} ₽/мес</div>
+              <div>от {{item.price_hour_with_weapon}} ₽/час </div>
+              <div> от {{item.price_with_weapon}} ₽/мес</div>
             </template>
           </b-table>
         </div>
         <div>
-          <h6>Стандарт</h6>
+          <h3>Стандарт</h3>
           <b-table class="text" hover :items="items2" :fields="fields">
             <template v-slot:cell(econom)="{item}">
-              <h4>{{item.name}}</h4>
+              <div class="text"><b>{{item.name}}</b></div>
               <span class="text_descr">{{item.descr_name}}</span>
             </template>
             <template v-slot:cell(tt)="{item}">
-              <div> от {{item.price}} ₽/час</div>
+              <div> от {{item.price_hour}} ₽/час</div>
               <div> от {{item.price}} ₽/мес</div>
             </template>
             <template v-slot:cell(ttr)="{item}">
-              <div>от {{item.price_with_weapon}} ₽/час </div>
-              <div> от {{item.price}} ₽/мес</div>
+              <div>от {{item.price_hour_with_weapon}} ₽/час </div>
+              <div> от {{item.price_with_weapon}} ₽/мес</div>
             </template>
           </b-table>
         </div>
         <div>
-          <h6>Премиум</h6>
+          <h3>Премиум</h3>
           <b-table class="text" hover :items="items3" :fields="fields">
             <template v-slot:cell(econom)="{item}">
-              <h4>{{item.name}}</h4>
+              <div class="text"><b>{{item.name}}</b></div>
               <span class="text_descr">{{item.descr_name}}</span>
             </template>
             <template v-slot:cell(tt)="{item}">
-              <div> от {{item.price}} ₽/час</div>
+              <div> от {{item.price_hour}} ₽/час</div>
               <div> от {{item.price}} ₽/мес</div>
             </template>
             <template v-slot:cell(ttr)="{item}">
-              <div>от {{item.price_with_weapon}} ₽/час </div>
-              <div> от {{item.price}} ₽/мес</div>
+              <div>от {{item.price_hour_with_weapon}} ₽/час </div>
+              <div> от {{item.price_with_weapon}} ₽/мес</div>
             </template>
           </b-table>
         </div>
       </div>
-
-
-
     </b-container>
   </div>
 </template>
 
 <script>
-  import Weapon_svg from "../tariffs_svg/weapon_svg";
-  import NoWeapon_svg from "../tariffs_svg/no_weapon_svg";
     export default {
       name: "tarrifs_content",
-      components: {Weapon_svg,NoWeapon_svg},
       data() {
         return {
-          fields: [{ label: "Режим работы", key: "econom" },
+          name_page:"Тарифы",
+          fields: [
+            { label: "Режим работы", key: "econom" },
             { label: "Невооруженная", key: "tt" },
-            { label: "Вооруженная", key: "ttr" },],
+            { label: "Вооруженная", key: "ttr" },
+          ],
           items: [
             {
               name:'Вахтовый режим',
               descr_name:'(смена 12 часов)',
+              price_hour:130,
               price:65000,
+              price_hour_with_weapon:150,
               price_with_weapon:70000,
             },
             {
               name:'Суточный режим',
               descr_name:'(двое через двое)',
+              price_hour:145,
               price:85000,
+              price_hour_with_weapon:165,
               price_with_weapon:90000,
             },
             {
               name:'Суточный режим',
               descr_name:'(сутки через двое)',
+              price_hour:150,
               price:95000,
+              price_hour_with_weapon:170,
               price_with_weapon:100000,
             },
             {
               name:'Дневной (ночной) режим',
               descr_name:'(12 часов, ежедневно)',
+              price_hour:140,
               price:75000,
+              price_hour_with_weapon:160,
               price_with_weapon:80000,
             },
             {
               name:'Дневной (ночной) режим',
-              descr_name:'(12 часов,  только рабочие дни)',
+              descr_name:'(12 часов, пн-пт)',
+              price_hour:135,
               price:70000,
+              price_hour_with_weapon:155,
               price_with_weapon:75000,
             },
           ],
@@ -108,31 +115,41 @@
             {
               name:'Вахтовый режим',
               descr_name:'(смена 12 часов)',
+              price_hour:140,
               price:80000,
+              price_hour_with_weapon:160,
               price_with_weapon:85000,
             },
             {
               name:'Суточный режим',
               descr_name:'(двое через двое)',
+              price_hour:165,
               price:105000,
+              price_hour_with_weapon:185,
               price_with_weapon:11000,
             },
             {
               name:'Суточный режим',
               descr_name:'(сутки через двое)',
+              price_hour:170,
               price:125000,
+              price_hour_with_weapon:190,
               price_with_weapon:130000,
             },
             {
               name:'Дневной (ночной) режим',
               descr_name:'(12 часов, ежедневно)',
+              price_hour:145,
               price:85000,
+              price_hour_with_weapon:165,
               price_with_weapon:90000,
             },
             {
               name:'Дневной (ночной) режим',
-              descr_name:'(12 часов,  только рабочие дни)',
+              descr_name:'(12 часов, пн-пт)',
+              price_hour:140,
               price:75000,
+              price_hour_with_weapon:160,
               price_with_weapon:80000,
             },
           ],
@@ -140,31 +157,41 @@
             {
               name:'Вахтовый режим',
               descr_name:'(смена 12 часов)',
+              price_hour:145,
               price:85000,
+              price_hour_with_weapon:165,
               price_with_weapon:90000,
             },
             {
               name:'Суточный режим',
               descr_name:'(двое через двое)',
+              price_hour:175,
               price:120000,
+              price_hour_with_weapon:195,
               price_with_weapon:125000,
             },
             {
               name:'Суточный режим',
               descr_name:'(сутки через двое)',
+              price_hour:185,
               price:135000,
+              price_hour_with_weapon:200,
               price_with_weapon:140000,
             },
             {
               name:'Дневной (ночной) режим',
               descr_name:'(12 часов, ежедневно)',
+              price_hour:165,
               price:100000,
+              price_hour_with_weapon:185,
               price_with_weapon:105000,
             },
             {
               name:'Дневной (ночной) режим',
-              descr_name:'(12 часов,  только рабочие дни)',
+              descr_name:'(12 часов, пн-пт)',
+              price_hour:145,
               price:85000,
+              price_hour_with_weapon:165,
               price_with_weapon:90000,
             },
           ]
@@ -173,53 +200,8 @@
     }
 </script>
 <style>
-  table{
-    background: rgba(219, 205, 205, 0.1);
-    box-shadow: 0 0 3px 1px #0f2538;
-  }
-  .table th{
-    background: rgba(8, 22, 33, 0.25) !important;
-
-  }
-  .table th{
-    background: rgba(8, 22, 33, 0.25) !important;
-  }
-  .table th, .table td {
-    padding: 0.35rem !important;
-    border: 1px solid rgba(250, 250, 250, 0.23) !important;
-    box-shadow: 0 0 1px 1px #0f2538;
-  }
-  th,tr td:nth-child(2),tr td:nth-child(3){
-    text-align: center !important;
-  }
 
 </style>
-<style scoped lang="scss">
-  h6{
-    text-align: center;
-  }
-  .flex-container > div {
-    width: 48%;
-    margin: 0 1% !important;
-    &:last-child{
-      margin-left: 25% !important;
-    }
-  }
-  img{
-    width: 5%;
-    margin: 0.5%;
-    //border-radius: 50%;
-    float: left;
-    box-shadow: none;
-    //background: rgba(255, 255, 255, 0.85);
-    filter: brightness(60%) !important
-  }
-  svg{
-    width: 32px;
-    height: 40px;
-    float: left;
-  }
-  p{
-    margin-top: 3.5%;
-  }
+<style scoped>
+
 </style>
