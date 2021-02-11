1d310637ba9702a0feb0bb185080aad603b280f5
AnigalHub <anigal1997@gmail.com>
2021-02-11 19:49:54 +0300

    изменены шрифты, заголовок + добавлен фильтр на фоновую картиннку

--- components/LandingPage/feedback.vue
+++ components/LandingPage/feedback.vue
@@ -8,7 +8,7 @@
           <b-col>
             <h2>Свяжитесь с нами!</h2>
             <a href="" class="call_telephone">+7(999)999-99-99</a>
-            <p class="text">Оставьте заявку или позвоните по телефону, мы ответим Вам на все интересующие Вас вопросы и представим индивидуальные решения для Вашего объекта.</p>
+            <p class="text1">Оставьте заявку или позвоните по телефону, мы ответим Вам на все интересующие Вас вопросы и представим индивидуальные решения для Вашего объекта.</p>
           </b-col>
         </b-row>
       </b-container>
@@ -27,21 +27,22 @@
   #Feedback{
     padding: 2% 0;
     background: url("../../static/LandingPage/55.jpg") 100% 100% no-repeat;
-    background-size: 100% 100%;
+    background-size: 100% 100%;  filter: brightness(80%);
   }
   h2{
-    margin-top: 15%;
+    margin-top: 13%;
+    font-size: 2.8rem;
     text-decoration: underline;
     text-align: center;
-    font-family: 'Viaoda Libre', cursive;
+    font-family: 'Yeseva One', cursive;
     padding-bottom: 2%;
     text-shadow: 2px 2.5px 2.5px rgb(15, 37, 56);
-    color: #cfdde8;
-    font-size: 2.8rem;
+    color: rgba(255, 255, 255, 0.86);
     background: transparent;
     font-weight: 500;
-    letter-spacing: normal;
+    letter-spacing: -.5px;
     padding-left: 0;
+
   }
 
   .call_telephone{
@@ -49,22 +50,24 @@
     text-align: center;
     transform: scale(1.35);
     display: block;
-    font-family: 'Texturina', serif;
-    text-shadow: 2px 5px 5px rgb(15, 37, 56);
     color: #cfdde8;
+    font-family: 'Vollkorn', serif;
+    font-weight: 500;
+    text-shadow: 2px 2px 2px rgb(15, 37, 56);
   }
 
-  .text{
+  .text1{
     margin-top: 2%;
-    font-family: 'Viaoda Libre', cursive;
-    font-weight: 500;
-    text-shadow: 1px 1px 2px rgb(15, 37, 56);
-    font-size: 1.7rem;
-    line-height: 2.5rem;
-    color: #ffffff;
+    margin-left: 15%;
+    font-size: 1.8rem;
+    font-family: 'Source Serif Pro', serif !important;
+    line-height: 2rem;
+    font-weight: 500; letter-spacing: -.85px;
+    transform: scale(1.2, 1.1);
+    color: #cfdde8;
   }
   .place_form{
-    background: rgba(255, 255, 255, 0.35);
+    background: rgba(255, 255, 255, 0.45);
   }
 
 </style>
