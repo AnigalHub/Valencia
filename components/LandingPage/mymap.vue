767e4108f9cf53ca239d3a0fd1709994eafca263
AnigalHub <anigal1997@gmail.com>
2021-02-12 19:38:57 +0300

    удален блок, в котором нет необходимости, стили вынесены в общий файл

--- components/LandingPage/mymap.vue
+++ components/LandingPage/mymap.vue
@@ -1,8 +1,6 @@
 <template>
     <div id="Map">
-      <div class="map">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8997.279426882762!2d37.87389096693673!3d55.68342660596789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab623148677f3%3A0xdecc2cd8ad0aba7b!2z0YPQuy4g0JrRgNCw0YHQvdCw0Y8sIDEsINCb0Y7QsdC10YDRhtGLLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LsuLCAxNDAwMDA!5e0!3m2!1sru!2sru!4v1609692951081!5m2!1sru!2sru" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-      </div>
     </div>
 </template>
 
@@ -12,33 +10,6 @@
     }
 </script>
 
-<style scoped lang="scss">
-  .map {
-    background-color: #0c1c42;
-    iframe {
-      filter: opacity(55%) brightness(95%) contrast(110%);
-      min-height: 400px;
-      height: auto;
-      width:100% !important;
-      border:0;
+<style scoped>
 
-      box-shadow: none !important;
-    }
-  }
-  @media screen and (max-width: 500px) {
-    .map {
-      iframe {
-        min-height: 250px !important;
-        border-radius: 0.625rem !important;
-      }
-    }
-  }
-  @media screen and (min-width: 500px)and (max-width: 768px) {
-    .map {
-      iframe {
-        min-height: 330px !important;
-        border-radius: 0.625rem !important;
-      }
-    }
-  }
 </style>
