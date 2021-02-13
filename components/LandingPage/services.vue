9dc267ab100e781a3eb3359e22e779958338eb38
AnigalHub <anigal1997@gmail.com>
2021-02-13 20:24:39 +0300

    изменен пордок услуг -  при адаптивности смотри лучше (из-за названий)

--- components/LandingPage/services.vue
+++ components/LandingPage/services.vue
@@ -3,14 +3,12 @@
       <b-container>
         <h2>Виды деятельности организации:</h2>
         <div class="flex-container">
-          <div v-for="(guard,index) in Guard" :key="index">
-            <router-link :to="guard.link">
-              <b-button>
-                <img :src="guard.src" :alt="guard.alt"/>
-                <p class="textLandingPage">{{guard.name_guard}}</p>
-              </b-button>
-            </router-link>
-          </div>
+          <router-link v-for="(guard,index) in Guard" :key="index" :to="guard.link">
+            <b-button>
+              <img :src="guard.src" :alt="guard.alt"/>
+              <p class="textLandingPage">{{guard.name_guard}}</p>
+            </b-button>
+          </router-link>
         </div>
         <Import_numbers/>
       </b-container>
@@ -27,9 +25,9 @@
         return{
           Guard:[
             {src:"LandingPage/21.jpg", alt:"охрана_организаций", name_guard:"Охрана организаций",link:'/services/guard_of_organizations/'},
+            {src:"LandingPage/22.jpg", alt:"охрана_недвижимости", name_guard:"Охрана недвижимости",link:'/services/guard_of_real_estate/'},
             {src:"LandingPage/24.jpg", alt:"охрана_объектов", name_guard:"Охрана объектов",link:'/services/object_guard/'},
             {src:"LandingPage/23.jpg", alt:"личная_охрана", name_guard:"Личная охрана",link:'/services/bodyguard'},
-            {src:"LandingPage/22.jpg", alt:"охрана_недвижимости", name_guard:"Охрана недвижимости",link:'/services/guard_of_real_estate/'},
             {src:"LandingPage/25.jpg", alt:"охрана_мероприятий", name_guard:"Охрана мероприятий",link:'/services/guard_of_events'},
             {src:"LandingPage/26.jpg", alt:"сопровождение_грузов", name_guard:"Сопровождение грузов",link:'/services/cargo_escort'},
           ]
