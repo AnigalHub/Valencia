4cef826410f0fc1b56ec05774e91e2742b5fce98
AnigalHub <anigal1997@gmail.com>
2021-02-13 20:26:48 +0300

    удалены стили - вынесены в общий файл, изменено название класса на текст

--- components/LandingPage/import_numbers.vue
+++ components/LandingPage/import_numbers.vue
@@ -4,7 +4,7 @@
         <div class="flex-container">
           <div v-for="(important_number, index) in important_numbers" :key="index">
             <div class="number">{{important_number.number}}</div>
-            <p class="text1" v-html="important_number. descr_number"></p>
+            <p class="textLandingPage" v-html="important_number. descr_number"></p>
           </div>
         </div>
       </b-container>
@@ -41,36 +41,5 @@
 
 <style scoped>
 
-  .number{
-    font-size:5.25rem;
-    text-align: center;
-    font-family: 'Vollkorn', serif;
-    font-weight: 400;
-    text-shadow: 2px 2px 2px rgb(15, 37, 56);
-    transform: scale(1.35);
-    letter-spacing: -4px;
-    margin-top: -5%;
-    color: white;
-  }
-  .flex-container{
-    margin-left: 8% !important;
-    margin-right: 8% !important;
-  }
-  .flex-container > div {
-    width: 20%;
-    padding: 0.25%;
-    margin: 1.5% 1.8% 0.5% 1.8% !important;
-  }
-  .text1{
-    font-family: 'Source Serif Pro', serif !important;
-    line-height: 1.55rem;
-    transform: scale(1.05);
-    margin-top: -5%;
-    font-size: 1.5rem;
-    text-align: center;
-    font-weight: 500;
-    letter-spacing: -.2px;
-    text-shadow: 0 2.5px 0  rgb(15, 37, 56) !important;
-    color: white;
-  }
+
 </style>
