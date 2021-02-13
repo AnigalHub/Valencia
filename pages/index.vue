e9a202f98516dadbf501504a439bedcd01d708e4
AnigalHub <anigal1997@gmail.com>
2021-02-13 03:37:31 +0300

    удалены лишние импорты меню и футора

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,32 @@
 <template>
-    <div id="index">
+    <div id="LandingPage">
       <Home/>
       <Type/>
-      <!-- <Advantages/> -->
       <Services/>
       <Feedback/>
       <Clients/>
       <Mymap/>
-      </div>
+    </div>
 </template>
 
 <script>
-import Navbar from "../components/navbar";
 import Home from "../components/LandingPage/home";
-import Advantages from "../components/LandingPage/advantages";
 import Type from "../components/LandingPage/type";
 import Services from "../components/LandingPage/services";
 import Feedback from "../components/LandingPage/feedback";
 import Clients from "../components/LandingPage/clients";
-import Myfooter from "../components/myfooter";
 import Mymap from "../components/LandingPage/mymap";
 export default {
-  components: {Mymap, Myfooter, Clients, Feedback, Services, Type, Advantages, Home, Navbar}
+  components: {Mymap, Clients, Feedback, Services, Type, Home}
 }
 </script>
 
 <style>
-  #index{
+  #LandingPage{
     background:linear-gradient( to top left, #304355, #768c9e,#304355);
+  }
+  #LandingPage img{
+    border: none;
   }
   .container-fluid{
     padding-left: 0 !important;
