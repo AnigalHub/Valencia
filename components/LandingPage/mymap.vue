65d9aef7c8e18b063738a43affa8c8e70901265e
AnigalHub <anigal1997@gmail.com>
2021-02-19 20:14:03 +0300

    ссылка на гугл карту берется из файла

--- components/LandingPage/mymap.vue
+++ components/LandingPage/mymap.vue
@@ -1,12 +1,18 @@
 <template>
     <div id="Map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8997.279426882762!2d37.87389096693673!3d55.68342660596789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab623148677f3%3A0xdecc2cd8ad0aba7b!2z0YPQuy4g0JrRgNCw0YHQvdCw0Y8sIDEsINCb0Y7QsdC10YDRhtGLLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LsuLCAxNDAwMDA!5e0!3m2!1sru!2sru!4v1609692951081!5m2!1sru!2sru" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <iframe :src="address.map"></iframe>
     </div>
 </template>
 
 <script>
+    import Address from "../../static/documents/address.json";
     export default {
-        name: "mymap"
+      name: "mymap",
+      data() {
+        return {
+          address: Address,
+        }
+      }
     }
 </script>
 
