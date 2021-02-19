e477ec2b80534e74942fe76c55c3be100bc79445
AnigalHub <anigal1997@gmail.com>
2021-02-19 20:14:47 +0300

    все данные, связанные с контактной информацией берутся из файла

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,7 +10,7 @@
               <component :is="information.svg"/>
             </b-col>
             <b-col>
-              <p class="text"><b>{{information.name}}:</b>{{information.description}}</p>
+              <p class="text"><b>{{information.name}}: </b>{{information.description}}</p>
             </b-col>
           </b-row>
         </b-col>
@@ -19,7 +19,7 @@
         </b-col>
       </b-row>
        <div class="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8997.279426882762!2d37.87389096693673!3d55.68342660596789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414ab623148677f3%3A0xdecc2cd8ad0aba7b!2z0YPQuy4g0JrRgNCw0YHQvdCw0Y8sIDEsINCb0Y7QsdC10YDRhtGLLCDQnNC-0YHQutC-0LLRgdC60LDRjyDQvtCx0LsuLCAxNDAwMDA!5e0!3m2!1sru!2sru!4v1609692951081!5m2!1sru!2sru" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+          <iframe :src="address.map"></iframe>
        </div>
       </b-container>
   </div>
@@ -33,18 +33,20 @@
     import UndergroundSVG from "../components/contact_svg/underground_svg";
     import AddressSVG from "../components/contact_svg/address_svg";
     import Send_form from "../components/secondary_pages/send_form";
+    import Address from "static/documents/address.json"
     export default {
       name: "contact",
       components: {Send_form, Name_page},
       data() {
         return {
+          address:Address,
           name_page:"Контакты",
           CompanyInformation:[
-            {svg:TelephoneSVG, name:"Телефон", description:" +7(999)999-99-99",},
-            {svg:EmailSVG, name:"Email", description:" valencia@yandex.ru",},
-            {svg:OpeningHoursSVG, name:"Часы работы", description:" Пн.-Пт., 9:00-18:00",},
-            {svg:UndergroundSVG, name:"Метро", description:" Жулебино, Котельники",},
-            {svg:AddressSVG, name:"Адрес", description:" Московская обл., г. Люберцы, улица Красная, 1, этаж 8, комната 24",},
+            {svg:TelephoneSVG, name:"Телефон", description:Address.phone,},
+            {svg:EmailSVG, name:"Email", description:Address.mail,},
+            {svg:OpeningHoursSVG, name:"Часы работы", description:Address.working_hours,},
+            {svg:UndergroundSVG, name:"Метро", description:Address.underground,},
+            {svg:AddressSVG, name:"Адрес", description:Address.address,},
           ]
         }
       }
