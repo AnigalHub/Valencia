50d14ee27af6ffafb83645dfe82da9faf48188b0
AnigalHub <anigal1997@gmail.com>
2021-02-20 13:09:08 +0300

    добавлено модальное окошко при клике на лицензию/благодарственное письмо

--- pages/licenses.vue
+++ pages/licenses.vue
@@ -10,13 +10,16 @@
         <h4 class="emphasized text">Лицензии:</h4>
         <p class="text">Действующие лицензии частного охранного предприятия “ООО Валенсия” на право оказания охранных услуг: </p>
         <div class="flex-container">
-          <img v-for="img in Licenses" :src="img.img_src" :alt="img.img_alt"/>
+          <img v-for="(img,index) in Licenses" :src="img.src" :alt="img.alt" :key="index" @click="showModal(img)"/>
         </div>
         <h4 class="emphasized text">Благодарные и рекомендательные письма:</h4>
         <p class="text">Наивысшей наградой являются благодарные и рекомендательные письма наших Клиентов о частном охранном предприятии “ООО Валенсия”:</p>
         <div class="flex-container">
-          <img v-for="img in ThanksgivingLetters" :src="img.img_src" :alt="img.img_alt"/>
+          <img v-for="img in ThanksgivingLetters" :src="img.src" :alt="img.alt" @click="showModal(img)"/>
         </div>
+        <b-modal ref="my-modal" id="img" size="lg" centered :title="selectedImages.name">
+          <img :src="selectedImages.src" :alt="selectedImages.alt"/>
+        </b-modal>
       </b-container>
   </div>
 </template>
@@ -28,6 +31,10 @@
       components: {Name_page},
       data() {
         return {
+         selectedImages:{
+           src:'',
+           alt:'',
+          },
           name_page:"Лицензии",
           TypesOfServices:[
             {type:"Защита жизни и здоровья граждан;",},
@@ -39,31 +46,63 @@
             {type:"Охрана объектов и/или имущества, а также обеспечение внутриобъектового и пропускного режимов на объектах, которые имеют особо важное значение для жизнедеятельности и безопасности государства и населения.",},
           ],
           Licenses:[
-            {img_src:"../licenses/license_1.jpg", img_alt:"Лицензия_1",},
-            {img_src:"../licenses/license_2.jpg", img_alt:"Лицензия_2",},
-            {img_src:"../licenses/license_3.jpg", img_alt:"Лицензия_3",},
-            {img_src:"../licenses/license_4.jpg", img_alt:"Лицензия_4",},
-            {img_src:"../licenses/license_5.jpg", img_alt:"Лицензия_5",},
-            {img_src:"../licenses/license_6.jpg", img_alt:"Лицензия_6",},
-            {img_src:"../licenses/license_7.jpg", img_alt:"Лицензия_7",},
-            {img_src:"../licenses/license_8.jpg", img_alt:"Лицензия_8",},
-            {img_src:"../licenses/license_9.jpg", img_alt:"Лицензия_9",},
-            {img_src:"../licenses/license_10.jpg", img_alt:"Лицензия_10",},
+            {src:"../licenses/license_1.jpg", alt:"Лицензия_1",name:"Лицензия 1",},
+            {src:"../licenses/license_2.jpg", alt:"Лицензия_2",name:"Лицензия 2",},
+            {src:"../licenses/license_3.jpg", alt:"Лицензия_3",name:"Лицензия 3",},
+            {src:"../licenses/license_4.jpg", alt:"Лицензия_4",name:"Лицензия 4",},
+            {src:"../licenses/license_5.jpg", alt:"Лицензия_5",name:"Лицензия 5",},
+            {src:"../licenses/license_6.jpg", alt:"Лицензия_6",name:"Лицензия 6",},
+            {src:"../licenses/license_7.jpg", alt:"Лицензия_7",name:"Лицензия 7",},
+            {src:"../licenses/license_8.jpg", alt:"Лицензия_8",name:"Лицензия 8",},
+            {src:"../licenses/license_9.jpg", alt:"Лицензия_9",name:"Лицензия 9",},
+            {src:"../licenses/license_10.jpg", alt:"Лицензия_10",name:"Лицензия 10",},
           ],
           ThanksgivingLetters:[
-            {img_src:"../licenses/thank_you_letter_1.jpg", img_alt:"Благодарственное_письмо_1",},
-            {img_src:"../licenses/thank_you_letter_2.jpg", img_alt:"Благодарственное_письмо_2",},
-            {img_src:"../licenses/thank_you_letter_3.jpg", img_alt:"Благодарственное_письмо_3",},
-            {img_src:"../licenses/thank_you_letter_4.jpg", img_alt:"Благодарственное_письмо_4",},
-            {img_src:"../licenses/thank_you_letter_5.jpg", img_alt:"Благодарственное_письмо_5",},
-            {img_src:"../licenses/thank_you_letter_6.jpg", img_alt:"Благодарственное_письмо_6",},
-            {img_src:"../licenses/thank_you_letter_7.jpg", img_alt:"Благодарственное_письмо_7",},
+            {src:"../licenses/thank_you_letter_1.jpg", alt:"Благодарственное_письмо_1",name:"Благодарственное письмо 1",},
+            {src:"../licenses/thank_you_letter_2.jpg", alt:"Благодарственное_письмо_2",name:"Благодарственное письмо 2",},
+            {src:"../licenses/thank_you_letter_3.jpg", alt:"Благодарственное_письмо_3",name:"Благодарственное письмо 3",},
+            {src:"../licenses/thank_you_letter_4.jpg", alt:"Благодарственное_письмо_4",name:"Благодарственное письмо 4",},
+            {src:"../licenses/thank_you_letter_5.jpg", alt:"Благодарственное_письмо_5",name:"Благодарственное письмо 5",},
+            {src:"../licenses/thank_you_letter_6.jpg", alt:"Благодарственное_письмо_6",name:"Благодарственное письмо 6",},
+            {src:"../licenses/thank_you_letter_7.jpg", alt:"Благодарственное_письмо_7",name:"Благодарственное письмо 7",},
           ]
         }
+      },
+      methods: {
+        showModal(img) {
+          this.selectedImages = img;
+          this.$refs['my-modal'].show()
+        },
       }
     }
 </script>
-
+<style>
+  .modal-dialog img{
+    width: 100% !important;
+    box-shadow: none !important;
+    filter: brightness(80%) contrast(120%)!important;
+  }
+  .close {
+    font-size: 2rem !important;
+  }
+  .modal-header .close {
+    padding: 1rem .5rem !important;
+  }
+  .modal-header{
+    padding: 0.5rem 1.5rem !important;
+  }
+  .modal-body {
+    padding:0 1rem 1rem 1rem!important;
+  }
+  .modal-footer {
+    display: none !important;
+  }
+  @media (min-width: 992px){
+    .modal-lg, .modal-xl {
+      max-width: 720px !important;
+    }
+  }
+</style>
 <style scoped lang="scss">
 
 </style>
