36f0059b8e7fb28b2c905af5f1ca1aa8e1e1de89
AnigalHub <anigal1997@gmail.com>
2021-04-11 22:40:45 +0300

    добавлен lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,31 +4,33 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: ' ЧОП "ООО Валенсия"',
+    lang: 'ru',
+    title: 'ЧОП "ООО Валенсия"',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'keywords', name: 'description', content: 'Валенсия, частное охранное предприятие, охрана организаций, охрана образовательных учреждений, охрана медицинских учреждений, охрана госучреждений, охрана объектов, охрана промышленных объектов, охрана объектов транспорта, охрана недвижимости, охрана коммерческих объектов, охрана частных объектов, личная охрана, охрана мероприятий, сопровождение грузов, Москва и Московская область, Люберцы, РФ.' },
+      { hid: 'description', name: 'description', content: '"ООО Валенсия" - частное охранное предприятие, обеспечивающее комплексную безопасность личности, предпринимательской деятельности, мероприятий, и всех, кто нуждается в охране на всей территории во всех субъектах Российской Федерации.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icons.png' },
       {
-        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap'
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap', media:'all', disabled:''
       },
       {
-        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap'
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap', media:'all', disabled:''
       },
       {
-        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Lobster&display=swap'
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Lobster&display=swap', media:'all', disabled:''
       },
       {
-        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap'
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap', media:'all', disabled:''
       },
       {
-        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap'
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap', media:'all', disabled:''
       },
       {
-        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap'
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap', media:'all', disabled:''
       },
     ]
   },
