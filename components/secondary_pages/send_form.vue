1587b438353a3f8636e83e6c95e16eb4fafe2c43
AnigalHub <anigal1997@gmail.com>
2021-04-12 22:15:20 +0300

    удалены input на почту и textarea

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -5,11 +5,7 @@
       <input v-model="feedback.name" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
       <label for="phone">Ваше телефон:</label>
       <input v-model="feedback.phone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="[+]7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
-      <label for="mail">Ваш E-mail:</label>
-      <input v-model="feedback.email" type="email" class="form-control" placeholder="Электронная почта" id="mail" name="mail" required="" data-validation-required-message="Пожалуйста, введите Ваш электронный адрес">
-      <label for="message">Ваше сообщение:</label>
-      <textarea v-model="feedback.message" class="form-control" placeholder="Опишите ваш запрос" id="message" name="message" required="" data-validation-required-message="Пожалуйста, введите сообщение"></textarea>
-      <button type="submit" class="justify-content-end">Отправить сообщение</button>
+      <button type="submit" class="justify-content-end">Отправить заявку</button>
     </form>
   </div>
 </template>
