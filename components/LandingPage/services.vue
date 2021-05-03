8589973d4c60b520e119a8e90819eea2e381b499
AnigalHub <anigal1997@gmail.com>
2021-05-03 13:17:20 +0300

    изменены пути до картинок + название класса для текста

--- components/LandingPage/services.vue
+++ components/LandingPage/services.vue
@@ -6,7 +6,7 @@
           <router-link v-for="(guard,index) in Guard" :key="index" :to="guard.link">
             <b-button>
               <img :src="guard.src" :alt="guard.alt"/>
-              <p class="textLandingPage">{{guard.name_guard}}</p>
+              <p class="text_Landing_page">{{guard.name_guard}}</p>
             </b-button>
           </router-link>
         </div>
@@ -24,12 +24,12 @@
       data(){
         return{
           Guard:[
-            {src:"LandingPage/21.jpg", alt:"охрана_организаций", name_guard:"Охрана организаций",link:'/services/guard_of_organizations/'},
-            {src:"LandingPage/22.jpg", alt:"охрана_недвижимости", name_guard:"Охрана недвижимости",link:'/services/guard_of_real_estate/'},
-            {src:"LandingPage/24.jpg", alt:"охрана_объектов", name_guard:"Охрана объектов",link:'/services/object_guard/'},
-            {src:"LandingPage/23.jpg", alt:"личная_охрана", name_guard:"Личная охрана",link:'/services/bodyguard'},
-            {src:"LandingPage/25.jpg", alt:"охрана_мероприятий", name_guard:"Охрана мероприятий",link:'/services/guard_of_events'},
-            {src:"LandingPage/26.jpg", alt:"сопровождение_грузов", name_guard:"Сопровождение грузов",link:'/services/cargo_escort'},
+            {src:"Landing_page/services_1.jpg", alt:"охрана_организаций", name_guard:"Охрана организаций",link:'/services/guard_of_organizations/'},
+            {src:"Landing_page/services_2.jpg", alt:"охрана_недвижимости", name_guard:"Охрана недвижимости",link:'/services/guard_of_real_estate/'},
+            {src:"Landing_page/services_3.jpg", alt:"охрана_объектов", name_guard:"Охрана объектов",link:'/services/object_guard/'},
+            {src:"Landing_page/services_4.jpg", alt:"личная_охрана", name_guard:"Личная охрана",link:'/services/bodyguard'},
+            {src:"Landing_page/services_5.jpg", alt:"охрана_мероприятий", name_guard:"Охрана мероприятий",link:'/services/guard_of_events'},
+            {src:"Landing_page/services_6.jpg", alt:"сопровождение_грузов", name_guard:"Сопровождение грузов",link:'/services/cargo_escort'},
           ]
         }
       }
