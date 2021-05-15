913db028b41f470e2736b5e2dc4814c9a9a8a119
AnigalHub <anigal1997@gmail.com>
2021-05-15 22:06:40 +0300

    изменен порт

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,11 +32,20 @@ export default {
       {
         rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap', media:'all', disabled:''
       },
+      {
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap', media:'all', disabled:''
+      },
+      {
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap', media:'all', disabled:''
+      },
+      {
+        rel: 'stylesheet' , href:'https://fonts.googleapis.com/css2?family=Kurale&family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap', media:'all', disabled:''
+      },
     ]
   },
   server:{
     host: "0.0.0.0",
-    port: 3000
+    port: 5000
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
