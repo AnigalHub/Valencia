dca2c30b3e90c1b431d54670e94caec36af6bc7d
AnigalHub <anigal1997@gmail.com>
2021-05-15 23:26:41 +0300

    название компании вынесено за row

--- components/LandingPage/home.vue
+++ components/LandingPage/home.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="Home">
       <b-container>
+        <div class="title_guard">Частное охранное предприятие "ООО Валенсия"</div>
         <b-row class="big_version">
           <b-col cols="4">
             <img src="../../static/Landing_page/guard.png" alt="охранник">
           </b-col>
           <b-col>
-            <div class="title_guard">Частное охранное предприятие <br> "ООО Валенсия"</div>
-            <div class="slogan">Безопасность частных лиц и объектов <br> любой сложности  на высшем уровне!</div>
+            <div class="slogan">Безопасность частных лиц и объектов  любой сложности  на высшем уровне!</div>
             <Advantages/>
           </b-col>
         </b-row>
