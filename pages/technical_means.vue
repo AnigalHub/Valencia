fc2d96a4546ec51c309d19cd90a9b35e36ad799c
AnigalHub <anigal1997@gmail.com>
2021-05-16 23:43:33 +0300

    добавлен row - картинки в col-5, описание в col - списком

--- pages/technical_means.vue
+++ pages/technical_means.vue
@@ -6,13 +6,19 @@
         <h6>Классификация технических средств охранников компании "ООО Валенсия":</h6>
         <div class="flex-container">
           <div v-for="mean in СlassificationTechnicalMeans" :key="mean.name_technical_means">
-            <img :src="mean.img_src" :alt="mean.img_alt"/>
             <p class="text emphasized">{{mean.name_technical_means}}</p>
-            <ul class="text">
-              <li v-for="paragraph in mean.list" :key="paragraph.index">
-                {{paragraph.paragraph}}
-              </li>
-            </ul>
+            <b-row>
+              <b-col cols="5">
+                <img :src="mean.img_src" :alt="mean.img_alt"/>
+              </b-col>
+              <b-col>
+                <ul class="text">
+                  <li v-for="paragraph in mean.list" :key="paragraph.index">
+                    {{paragraph.paragraph}}
+                  </li>
+                </ul>
+              </b-col>
+            </b-row>
           </div>
         </div>
       </b-container>
@@ -29,7 +35,7 @@
           name_page:"Технические средства",
           СlassificationTechnicalMeans:[
             {
-              name_technical_means:"Пожарная сигнализация:",
+              name_technical_means:"Пожарная сигнализация",
               img_src:"../technical_means/fire_alarm.jpg",
               img_alt:"пожарная_сигнализация",
               list:[
@@ -40,7 +46,7 @@
               ],
             },
             {
-              name_technical_means:"Система контроля управления доступом:",
+              name_technical_means:"Система контроля управления доступом",
               img_src:"../technical_means/access_control.jpg",
               img_alt:"контроль_доступа",
               list:[
@@ -52,7 +58,7 @@
               ],
             },
             {
-              name_technical_means:"Охранная сигнализация:",
+              name_technical_means:"Охранная сигнализация",
               img_src:"../technical_means/guard_alarm.png",
               img_alt:"охранная_сигнализация",
               list:[
@@ -64,7 +70,7 @@
               ],
             },
             {
-              name_technical_means:"Система видеоналюдения:",
+              name_technical_means:"Система видеоналюдения",
               img_src:"../technical_means/cctv.jpg",
               img_alt:"видеонаблюдение",
               list:[
@@ -75,7 +81,7 @@
               ],
             },
             {
-              name_technical_means:"Технические средства пультовой охраны:",
+              name_technical_means:"Технические средства пультовой охраны",
               img_src:"../technical_means/console_guard.jpg",
               img_alt:"пультовая_охрана",
               list:[
@@ -85,12 +91,13 @@
               ],
             },
             {
-              name_technical_means:"Тревожная сигнализация:",
+              name_technical_means:"Тревожная сигнализация",
               img_src:"../technical_means/alarm_signaling.jpg",
               img_alt:"тревожная_сигнализация",
               list:[
                 {paragraph:"контрольная панель;"},
                 {paragraph:"механический ключ разблокировки;"},
+                {paragraph:"передатчик сигнала;"},
                 {paragraph:"тревожная кнопка."},
               ],
             },
