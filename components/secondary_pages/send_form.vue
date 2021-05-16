3b911788223245de902decdddd69d0d5a4609249
AnigalHub <anigal1997@gmail.com>
2021-05-17 00:06:41 +0300

    добавлен checkbox и фраза об обработке информации

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -5,6 +5,9 @@
       <input v-model="feedback.name" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
       <label for="phone">Ваше телефон:</label>
       <input v-model="feedback.phone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="[+]7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
+      <p class="text_descr" style="text-align: center;font-weight: bold">
+        <input type="checkbox" class="checkbox"/>
+       Нажимая кнопку, вы соглашаетесь на обработку персональных данных.</p>
       <button type="submit" class="justify-content-end">Отправить заявку</button>
     </form>
   </div>
