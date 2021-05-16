98db6a23555e056efccca8de3a4257cd9b0e3626
AnigalHub <anigal1997@gmail.com>
2021-05-16 23:41:19 +0300

    переписано также, но через row

--- components/LandingPage/type.vue
+++ components/LandingPage/type.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container-inner" id="Type">
-    <router-link :to="category.link" class="section" v-for="(category, index) in Categories" :key="index">
-      <div class="content">
+  <b-row>
+      <router-link :to="category.link" class="col" v-for="(category, index) in Categories" :key="index">
+        <img :src="category.img_src">
         <div class="container">
           <h2>{{category.category}}</h2>
           <div class="text_Landing_page">{{category.descr_category}}</div>
         </div>
-      </div>
-      <div class="overlay"></div>
-    </router-link>
+      </router-link>
+    </b-row>
   </div>
 </template>
 
@@ -19,16 +19,19 @@
         return{
           Categories:[
             {
+              img_src:"Landing_page/22.jpg",
               link:"/company/",
               category:"Информация для клиентов",
               descr_category:"(принципы и этапы работы)",
             },
             {
+              img_src:"Landing_page/222.jpg",
               link:"/tariffs/",
               category:"Прайс-лист на все виды услуг",
               descr_category:"(актуальные цены по тарифам)",
             },
             {
+              img_src:"Landing_page/2222.jpg",
               link:"/licenses/",
               category:"Впечатление о работе с нами",
               descr_category:"(благодарности и рекомендации)",
