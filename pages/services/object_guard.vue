c8b3738c2a66a2b31632a772f3c77e6650eed9d1
AnigalHub <anigal1997@gmail.com>
2021-05-23 21:38:29 +0300

    изменен класс css, название компонента - без импорта, убраны svg

--- pages/services/object_guard.vue
+++ pages/services/object_guard.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="object_guard" class="page services services_with_types">
-      <Name_page :name_page="name_page"/>
       <b-container>
+        <h1>{{name_page}}</h1>
         <img src="../../static/services/object_guard/object_guard.jpg" alt="охрана_объектов" class="first_img"/>
         <p class="text"><span class="emphasized">Охрана объектов </span>- комплексное обеспечение защиты производственного или коммерческого объекта, товарно-материальных ценностей и защиту здоровья сотрудников объекта. Для обеспечения безопасности объекта разрабатывается проект со всеми особенностями и характером деятельности бизнеса и его графиком работы.</p>
         <h4 class="text"><u>При составлении проекта работы охранников учитывается:</u></h4>
@@ -10,7 +10,7 @@
         </ul>
         <h6 class="block_two">Разновидности охраны объектов:</h6>
         <div class="flex-container">
-          <div class="type_guard_beautiful_block"  v-for="guard in TypesOfGuard" :key="guard.index">
+          <div class="beautiful_block"  v-for="guard in TypesOfGuard" :key="guard.index">
             <NuxtLink :to="guard.link">
               <p class="emphasized">{{guard.name}}</p>
               <b-row>
@@ -32,12 +32,10 @@
 </template>
 
 <script>
-  import Name_page from "../../components/secondary_pages/name_page";
-  import GuardSVG from "../../components/secondary_pages/shield_svg";
   import Tariffs from "../tariffs";
     export default {
       name: "object_guard",
-      components: {Tariffs, Name_page},
+      components: {Tariffs},
       data() {
         return {
           name_page:"Охрана объектов",
@@ -52,7 +50,6 @@
             {
               link:"/services/_object_guard/guard_of_industrial_facilities",
               name:"Охрана промышленных объектов",
-              svg:GuardSVG,
               img_src:"../object_guard/checkpoint.png",
               img_alt:"охрана_коммерческих_объектов",
               list:[
@@ -67,7 +64,6 @@
             {
               link:"/services/_object_guard/guard_of_transport_facilities",
               name:"Охрана объектов транспорта",
-              svg:GuardSVG,
               img_src:"../object_guard/barrier.jpg",
               img_alt:"охрана_частных_объектов",
               list:[
@@ -77,7 +73,8 @@
                 {guard:"охрана гаражных комплексов;"},
                 {guard:"охрана автостоянок;"},
                 {guard:"охрана аэропортов."},
-              ]},
+              ]
+            },
           ],
         }
       }
