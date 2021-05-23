b854c7870b3259ac725c4c39f8c4a120ebf35f85
AnigalHub <anigal1997@gmail.com>
2021-05-23 21:44:54 +0300

    название компонета - не через импорт, добавлен заголовок, удалены svg

--- pages/services/guard_of_real_estate.vue
+++ pages/services/guard_of_real_estate.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="guard_of_real_estate" class="page services services_with_types">
-    <Name_page :name_page="name_page"/>
     <b-container>
-     <img src="../../static/services/guard_of_real_estate/guard_of_real_estate.jpg" alt="охрана_недвижимости" class="first_img"/>
+      <h1>{{name_page}}</h1>
+      <img src="../../static/services/guard_of_real_estate/guard_of_real_estate.jpg" alt="охрана_недвижимости" class="first_img"/>
       <p class="text"><span class="emphasized">Профессиональная охрана недвижимости </span> позволяет владельцам объекта работать и отдыхать спокойно. Наша организация гарантирует полную безопасность посетителей и персонала, а также сохранность имущества всего здания.</p>
-      <p class="text"><u>Методы работы охраны недвижимости подбираются отдельно под каждый объект, учитывая:</u></p>
+      <h4 class="text"><u>Методы работы охраны недвижимости подбираются отдельно под каждый объект, учитывая:</u></h4>
       <ul class="text">
         <li v-for="name_metod in WorkingMethods" :key="name_metod.index">{{name_metod.name_metod}}</li>
       </ul>
       <h6 class="block_two">Разновидности охраны недвижимости:</h6>
       <div class="flex-container">
-        <div class="type_guard_beautiful_block" v-for="guard in TypesOfGuard" :key="guard.index">
+        <div class="beautiful_block" v-for="guard in TypesOfGuard" :key="guard.index">
           <NuxtLink :to="guard.link">
-            <component :is="guard.svg"/><span class="name_type_guard">{{guard.name}}</span>
+            <p class="emphasized">{{guard.name}}</p>
             <b-row>
               <b-col cols="5">
                 <img :src="guard.img_src" :alt="guard.img_alt"/>
@@ -26,18 +26,16 @@
           </NuxtLink>
         </div>
       </div>
-      <Tarrifs_content/>
+      <Tariffs/>
     </b-container>
   </div>
 </template>
 
 <script>
-    import Tarrifs_content from "../../components/secondary_pages/tarrifs_content";
-    import Name_page from "../../components/secondary_pages/name_page";
-    import GuardSVG from "../../components/secondary_pages/shield_svg";
+    import Tariffs from "../tariffs";
     export default {
       name: "guard_of_real_estate",
-      components: {Name_page, Tarrifs_content},
+      components: {Tariffs},
       data() {
         return {
           name_page: "Охрана недвижимости",
@@ -53,7 +51,6 @@
             {
               link:"/services/_guard_of_real_estate/guard_of_commercial_objects",
               name:"Охрана коммерческих объектов",
-              svg:GuardSVG,
               img_src:"./guard_of_commercial_object.jpg",
               img_alt:"охрана_коммерческих_объектов",
               list:[
@@ -68,7 +65,6 @@
             {
               link:"/services/_guard_of_real_estate/guard_private_property",
               name:"Охрана частных объектов",
-              svg:GuardSVG,
               img_src:"./panel_protection.jpg",
               img_alt:"охрана_частных_объектов",
               list:[
