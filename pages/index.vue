c40d25c956c4229f20bb4b2492f3f863b79c147a
AnigalHub <anigal1997@gmail.com>
2021-05-24 20:37:56 +0300

    убраны стили

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="Landing_page">
       <Home/>
-
       <Services/>
       <Type/>
       <Feedback/>
@@ -12,26 +11,16 @@
 
 <script>
 import Home from "../components/LandingPage/home";
-import Type from "../components/LandingPage/type";
 import Services from "../components/LandingPage/services";
+import Type from "../components/LandingPage/type";
 import Feedback from "../components/LandingPage/feedback";
 import Clients from "../components/LandingPage/clients";
 import Mymap from "../components/LandingPage/mymap";
 export default {
-  components: {Mymap, Clients, Feedback, Services, Type, Home}
+  components: {Mymap, Clients, Feedback, Type, Services, Home}
 }
 </script>
 
 <style>
-  .container-fluid{
-    padding-left: 0 !important;
-    padding-right: 0 !important;
-  }
-  .container{
-    padding-right: 15px !important;
-    padding-left: 15px !important;
-  }
-  .row {
-    margin: 0 !important;
-  }
+  
 </style>
