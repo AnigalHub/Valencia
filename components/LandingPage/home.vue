5b84eb9613ec56e0345f0537e145f19fd9babbe6
AnigalHub <anigal1997@gmail.com>
2021-05-25 13:58:49 +0300

    убраны версии (мобильная и компьютерная)

--- components/LandingPage/home.vue
+++ components/LandingPage/home.vue
@@ -2,6 +2,7 @@
     <div id="Home">
       <b-container>
         <div class="title_guard">Частное охранное предприятие "ООО Валенсия"</div>
+        <div class="slogan">Безопасность частных лиц и объектов  любой сложности  на Высшем уровне!</div>
         <b-row class="big_version">
           <b-col cols="4">
             <img src="../../static/Landing_page/guard.png" alt="охранник">
@@ -11,19 +12,6 @@
             <Advantages/>
           </b-col>
         </b-row>
-        <div class="mobile_version">
-          <div class="title_guard">Частное охранное предприятие <br> "ООО Валенсия"</div>
-          <div class="slogan">Безопасность частных лиц и объектов <br> любой сложности  на Высшем уровне!</div>
-          <b-row>
-            <b-col cols="4">
-              <img src="../../static/Landing_page/guard.png" alt="охранник">
-            </b-col>
-            <b-col>
-              <div class="slogan">Безопасность частных  лиц  и объектов  любой сложности на Высшем уровне!</div>
-              <Advantages/>
-            </b-col>
-          </b-row>
-        </div>
       </b-container>
     </div>
 </template>
