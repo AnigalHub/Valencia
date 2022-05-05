f8b37fa16f02b007797b1d313bb5e95fd7efb60e
AnigalHub <anigal1997@gmail.com>
2022-05-06 00:16:10 +0300

    изменен href: добавлен tel

--- components/navbar.vue
+++ components/navbar.vue
@@ -5,7 +5,7 @@
         <component :is="svg"/>
         <span class="name_brand">"{{information.name_company}}"</span>
       </b-navbar-brand>
-      <a :href="information.phone" class="telephone"> {{information.phone}} </a>
+      <a href="tel:+information.phone" class="telephone"> {{information.phone}} </a>
       <b-navbar-toggle target="navbar-toggle-collapse">
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"  width="35" height="25" viewBox="0 0 172 172" style=" fill:#000000;">
           <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
