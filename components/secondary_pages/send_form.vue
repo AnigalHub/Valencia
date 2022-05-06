64ac0119d53fd46cfd5f24351288273de7e530ec
AnigalHub <anigal1997@gmail.com>
2022-05-06 23:20:30 +0300

    изменен падеж в тексте

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -3,7 +3,7 @@
     <form name="sentMessage" id="Form" @submit.prevent="SendFeedback">
       <label for="name">Ваше имя:</label>
       <input v-model="feedback.name" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
-      <label for="phone">Ваше телефон:</label>
+      <label for="phone">Ваш телефон:</label>
       <input v-model="feedback.phone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="[+]7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
       <p class="text_descr" style="text-align: center;font-weight: bold">
         <input type="checkbox" class="checkbox"/>
