6bec254f2d8aa0cef378b6b3af3e6b2366ff429b
AnigalHub <anigal1997@gmail.com>
2022-05-06 23:20:15 +0300

    изменены отступы

--- components/LandingPage/import_numbers.vue
+++ components/LandingPage/import_numbers.vue
@@ -15,14 +15,14 @@
     export default {
         name: "import_numbers",
       data(){
-          return{
-            important_numbers:[
-              {number:15, descr_number: "Лет на рынке <br> безопасности",},
-              {number:564, descr_number: "Профессиональных сотрудников",},
-              {number:50, descr_number: "Благодарностей <br> и рекомендаций",},
-              {number:74, descr_number: "Постоянных <br> клиентов",},
-            ],
-          }
+        return{
+          important_numbers:[
+            {number:15, descr_number: "Лет на рынке <br> безопасности",},
+            {number:564, descr_number: "Профессиональных сотрудников",},
+            {number:50, descr_number: "Благодарностей <br> и рекомендаций",},
+            {number:74, descr_number: "Постоянных <br> клиентов",},
+          ],
+        }
       }
     }
 </script>
