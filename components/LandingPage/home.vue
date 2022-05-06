8da0d3e43cf50893806a07b754e6d11c9a492ebc
AnigalHub <anigal1997@gmail.com>
2022-05-06 23:20:00 +0300

    добавлено название компании с помощью интерполяции

--- components/LandingPage/home.vue
+++ components/LandingPage/home.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="Home">
       <b-container>
-        <div class="title_guard">Частное охранное предприятие <nobr> "Пересвет" </nobr></div>
+        <div class="title_guard">Частное охранное предприятие <nobr> "{{information.name_company}}" </nobr></div>
         <b-row class="big_version">
           <b-col cols="4">
             <img src="../../static/Landing_page/Home/guard.png" alt="охранник">
@@ -17,9 +17,15 @@
 
 <script>
     import Advantages from "./advantages";
+    import Information from "../../static/documents/information.json";
     export default {
       name: "home",
       components: {Advantages},
+      data(){
+        return{
+          information:Information,
+        }
+      }
     }
 </script>
 <style scoped>
