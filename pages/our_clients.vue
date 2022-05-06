6eb1df80e2126813293ed90bbf3478a2f847b5f1
AnigalHub <anigal1997@gmail.com>
2022-05-06 23:14:20 +0300

    добавлена интерполяция на название компании

--- pages/our_clients.vue
+++ pages/our_clients.vue
@@ -2,7 +2,7 @@
   <div id="our_clients" class="page">
     <b-container>
       <h1>{{name_page}}</h1>
-      <p class="text">За время существования частного охранного предприятия "Пересвет" были заключены долгосрочные и надежные отношения с различными организациями: государственными учреждениями (детские сады, школы, вузы), бизнес-центрами, ритейлерами, торговыми центрами, букмекерскими конторами, строительными компаниями и т.д.</p>
+      <p class="text">За время существования частного охранного предприятия <span class="emphasized"> "{{information.name_company}}" </span> были заключены долгосрочные и надежные отношения с различными организациями: государственными учреждениями (детские сады, школы, вузы), бизнес-центрами, ритейлерами, торговыми центрами, букмекерскими конторами, строительными компаниями и т.д.</p>
       <h6>Предприятия, которые выбрали наши услуги:</h6>
       <div class="flex-container">
         <div v-for="client in Clients" :key="client.index">
@@ -15,11 +15,13 @@
 </template>
 
 <script>
+    import Information from "static/documents/information.json"
     export default {
       name: "our_clients",
       data() {
         return {
           name_page:"Наши клиенты",
+          information:Information,
           Clients:[
             {name_client:"МОУ Гимназия №46", img_src:"../our_clients/MOU_Gymnasium_46.jpg", img_alt:"моу_гимназия_46",},
             {name_client:"МОУ СОШ №25", img_src:"../our_clients/MOU_SOSH_25.jpg", img_alt:"моу_сош_25",},
