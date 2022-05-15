de7d8afc04ae467cb027bfa0a636c63df3b0b6bb
AnigalHub <anigal1997@gmail.com>
2022-05-16 01:04:12 +0300

    удалены placeholder - из-за маски нет необходимости

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -2,9 +2,9 @@
   <div>
     <form name="sentMessage" id="Form">
       <label for="name">Ваше имя:</label>
-      <input v-model="feedback.name" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
+      <input v-model="feedback.name" type="text" class="form-control" id="name" name="name" data-validation-required-message="Пожалуйста, введите Ваше имя">
       <label for="phone">Ваш телефон:</label>
-      <input v-model="feedback.phone" type="tel" class="form-control" v-mask="'+7 (###) ###-##-##'" placeholder="+7(___)___-__-__" pattern="[+]7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
+      <input v-model="feedback.phone" type="tel" class="form-control" v-mask="'+7 (###) ###-##-##'" id="phone" name="phone" data-validation-required-message="Пожалуйста, введите Номер телефона">
       <p class="text_descr" style="text-align: center;font-weight: bold">
         <input type="checkbox" class="checkbox"/>
        Нажимая кнопку, вы соглашаетесь на обработку персональных данных.</p>
