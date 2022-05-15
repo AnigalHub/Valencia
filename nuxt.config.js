6b11f3e2d6af96c8c043d4914811bd940d6846d8
AnigalHub <anigal1997@gmail.com>
2022-05-16 00:51:47 +0300

    добавлен плагин: '@plugins/v-mask.js'

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,9 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [
+    '@plugins/v-mask.js',
+  ],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
