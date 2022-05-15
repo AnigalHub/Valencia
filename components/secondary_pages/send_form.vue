9fb785bcd2ceaaaddaaca3baa6dfd2e04526140c
AnigalHub <anigal1997@gmail.com>
2022-05-16 01:12:27 +0300

    удален font-weight

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -5,9 +5,8 @@
       <input v-model="feedback.name" type="text" class="form-control" id="name" name="name" data-validation-required-message="Пожалуйста, введите Ваше имя">
       <label for="phone">Ваш телефон:</label>
       <input v-model="feedback.phone" type="tel" class="form-control" v-mask="'+7 (###) ###-##-##'" id="phone" name="phone" data-validation-required-message="Пожалуйста, введите Номер телефона">
-      <p class="text_descr" style="text-align: center;font-weight: bold">
-        <input type="checkbox" class="checkbox"/>
-       Нажимая кнопку, вы соглашаетесь на обработку персональных данных.</p>
+      <p class="text_descr" style="text-align: center;">
+        <input type="checkbox" class="checkbox"/> Нажимая кнопку, вы соглашаетесь на обработку персональных данных.</p>
       <button type="submit">Отправить заявку</button>
     </form>
   </div>
