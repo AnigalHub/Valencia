71b2cf64838d9c2ea04290d65e30e1a75ff4bda4
AnigalHub <anigal1997@gmail.com>
2022-05-15 23:56:12 +0300

    изменен v-slot:cell

--- pages/tariffs.vue
+++ pages/tariffs.vue
@@ -8,15 +8,15 @@
           <img src="../static/tariffs/type_1.png" alt="тип_1"/>
           <p class="text ">Сотрудники с хорошим телосложением, имеющие  разряд не выше 5-го, с опытом работы от 1 года, обучены для выполнения общенаблюдательных функций и обеспечения пропускного режима</p>
           <b-table class="text" hover :items="items" :fields="fields">
-            <template v-slot:cell(econom)="{item}">
+            <template v-slot:cell(mode)="{item}">
               <div class="text">{{item.name}}</div>
               <span class="text_descr">{{item.descr_name}}</span>
             </template>
-            <template v-slot:cell(tt)="{item}">
+            <template v-slot:cell(unarmed)="{item}">
               <div> от {{item.price_hour}} ₽/час</div>
               <div> от {{item.price}} ₽/мес</div>
             </template>
-            <template v-slot:cell(ttr)="{item}">
+            <template v-slot:cell(armed)="{item}">
               <div>от {{item.price_hour_with_weapon}} ₽/час </div>
               <div> от {{item.price_with_weapon}} ₽/мес</div>
             </template>
@@ -27,15 +27,15 @@
           <img src="../static/tariffs/type_2.png" alt="тип_2"/>
           <p class="text ">Сотрудники, со среднем телосложением, имеющие  разряд 5-6, с опытом работы от 3-лет, обучены для выполнения контрольно-пропускного режима и работы с техническими средствами</p>
           <b-table class="text" hover :items="items2" :fields="fields">
-            <template v-slot:cell(econom)="{item}">
+            <template v-slot:cell(mode)="{item}">
               <div class="text">{{item.name}}</div>
               <span class="text_descr">{{item.descr_name}}</span>
             </template>
-            <template v-slot:cell(tt)="{item}">
+            <template v-slot:cell(unarmed)="{item}">
               <div> от {{item.price_hour}} ₽/час</div>
               <div> от {{item.price}} ₽/мес</div>
             </template>
-            <template v-slot:cell(ttr)="{item}">
+            <template v-slot:cell(armed)="{item}">
               <div>от {{item.price_hour_with_weapon}} ₽/час </div>
               <div> от {{item.price_with_weapon}} ₽/мес</div>
             </template>
@@ -46,15 +46,15 @@
           <img src="../static/tariffs/type_3.png" alt="тип_3"/>
           <p class="text">Сотрудники, строго до 45-лет, крепкого телосложения и презентабельной внешности, имеющие 6-ой разряд, с опытом работы от 5-лет, обучены для выполнения особо сложных задач</p>
           <b-table class="text" hover :items="items3" :fields="fields">
-            <template v-slot:cell(econom)="{item}">
+            <template v-slot:cell(mode)="{item}">
               <div class="text">{{item.name}}</div>
               <span class="text_descr">{{item.descr_name}}</span>
             </template>
-            <template v-slot:cell(tt)="{item}">
+            <template v-slot:cell(unarmed)="{item}">
               <div> от {{item.price_hour}} ₽/час</div>
               <div> от {{item.price}} ₽/мес</div>
             </template>
-            <template v-slot:cell(ttr)="{item}">
+            <template v-slot:cell(armed)="{item}">
               <div>от {{item.price_hour_with_weapon}} ₽/час </div>
               <div> от {{item.price_with_weapon}} ₽/мес</div>
             </template>
