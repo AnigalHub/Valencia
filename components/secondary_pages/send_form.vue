6389135dd37a92e5385f299a6e4b4561847597e1
AnigalHub <anigal1997@gmail.com>
2022-05-16 00:53:42 +0300

    добавлена маска на номер

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -4,7 +4,7 @@
       <label for="name">Ваше имя:</label>
       <input v-model="feedback.name" type="text" class="form-control" placeholder="ФИО" id="name" name="name" required="" data-validation-required-message="Пожалуйста, введите Ваше имя">
       <label for="phone">Ваш телефон:</label>
-      <input v-model="feedback.phone" type="tel" class="form-control" placeholder="+7(xxx)xxx-xx-xx" pattern="[+]7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
+      <input v-model="feedback.phone" type="tel" class="form-control" v-mask="'+7 (###) ###-##-##'" placeholder="+7(___)___-__-__" pattern="[+]7\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}" id="phone" name="phone" required="" data-validation-required-message="Пожалуйста, введите Номер телефона">
       <p class="text_descr" style="text-align: center;font-weight: bold">
         <input type="checkbox" class="checkbox"/>
        Нажимая кнопку, вы соглашаетесь на обработку персональных данных.</p>
