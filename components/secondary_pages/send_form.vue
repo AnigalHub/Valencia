555dd6aa7dba5fab7fc22d2a30bb7ff3d0287a11
AnigalHub <anigal1997@gmail.com>
2022-05-16 01:36:25 +0300

    удалены id

--- components/secondary_pages/send_form.vue
+++ components/secondary_pages/send_form.vue
@@ -2,9 +2,9 @@
   <div>
     <form name="sentMessage" id="Form">
       <label for="name">Ваше имя:</label>
-      <input v-model="feedback.name" class="form-control" id="name" name="name" data-validation-required-message="Пожалуйста, введите Ваше имя">
+      <input v-model="feedback.name" class="form-control" id="name"  data-validation-required-message="Пожалуйста, введите Ваше имя">
       <label for="phone">Ваш телефон:</label>
-      <input v-model="feedback.phone" class="form-control" v-mask="'+7 (###) ###-##-##'" id="phone" name="phone" data-validation-required-message="Пожалуйста, введите Номер телефона">
+      <input v-model="feedback.phone" class="form-control" v-mask="'+7 (###) ###-##-##'" id="phone" data-validation-required-message="Пожалуйста, введите Номер телефона">
       <p class="text_descr" style="text-align: center;">
         <input type="checkbox" class="checkbox"/> Нажимая кнопку, вы соглашаетесь на обработку персональных данных.
       </p>
