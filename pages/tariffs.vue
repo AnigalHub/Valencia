ec061102aa46418867836c11f4f5ce376557975a
AnigalHub <anigal1997@gmail.com>
2022-05-16 00:21:13 +0300

    изменены :items

--- pages/tariffs.vue
+++ pages/tariffs.vue
@@ -7,7 +7,7 @@
           <h3>Эконом</h3>
           <img src="../static/tariffs/type_1.png" alt="тип_1"/>
           <p class="text ">Сотрудники с хорошим телосложением, имеющие  разряд не выше 5-го, с опытом работы от 1 года, обучены для выполнения общенаблюдательных функций и обеспечения пропускного режима</p>
-          <b-table class="text" hover :items="items" :fields="fields">
+          <b-table class="text" hover :items="tarrifs.econom" :fields="fields">
             <template v-slot:cell(mode)="{item}">
               <div class="text">{{item.name}}</div>
               <span class="text_descr">{{item.descr_name}}</span>
@@ -26,7 +26,7 @@
           <h3>Стандарт</h3>
           <img src="../static/tariffs/type_2.png" alt="тип_2"/>
           <p class="text ">Сотрудники, со среднем телосложением, имеющие  разряд 5-6, с опытом работы от 3-лет, обучены для выполнения контрольно-пропускного режима и работы с техническими средствами</p>
-          <b-table class="text" hover :items="items2" :fields="fields">
+          <b-table class="text" hover :items="tarrifs.standart" :fields="fields">
             <template v-slot:cell(mode)="{item}">
               <div class="text">{{item.name}}</div>
               <span class="text_descr">{{item.descr_name}}</span>
@@ -45,7 +45,7 @@
           <h3>Премиум</h3>
           <img src="../static/tariffs/type_3.png" alt="тип_3"/>
           <p class="text">Сотрудники, строго до 45-лет, крепкого телосложения и презентабельной внешности, имеющие 6-ой разряд, с опытом работы от 5-лет, обучены для выполнения особо сложных задач</p>
-          <b-table class="text" hover :items="items3" :fields="fields">
+          <b-table class="text" hover :items="tarrifs.premium" :fields="fields">
             <template v-slot:cell(mode)="{item}">
               <div class="text">{{item.name}}</div>
               <span class="text_descr">{{item.descr_name}}</span>
@@ -66,142 +66,18 @@
 </template>
 
 <script>
+  import Tarrifs from "static/documents/tarrifs.json"
   export default {
     name: "tariffs",
     data() {
       return {
         name_page:"Тарифы",
+        tarrifs:Tarrifs,
         fields: [
           { label: "Режим", key: "mode" },
           { label: "Невооруженная", key: "unarmed" },
           { label: "Вооруженная", key: "armed" },
         ],
-        items: [
-          {
-            name:'Вахтовый',
-            descr_name:'(12 ч.)',
-            price_hour:130,
-            price:65000,
-            price_hour_with_weapon:150,
-            price_with_weapon:70000,
-          },
-          {
-            name:'Суточный',
-            descr_name:'(2/2)',
-            price_hour:145,
-            price:85000,
-            price_hour_with_weapon:165,
-            price_with_weapon:90000,
-          },
-          {
-            name:'Суточный',
-            descr_name:'(1/2)',
-            price_hour:150,
-            price:95000,
-            price_hour_with_weapon:170,
-            price_with_weapon:100000,
-          },
-          {
-            name:'Дневной (ночной)',
-            descr_name:'(12 ч., пн-вс)',
-            price_hour:140,
-            price:75000,
-            price_hour_with_weapon:160,
-            price_with_weapon:80000,
-          },
-          {
-            name:'Дневной (ночной)',
-            descr_name:'(12 ч., пн-пт)',
-            price_hour:135,
-            price:70000,
-            price_hour_with_weapon:155,
-            price_with_weapon:75000,
-          },
-        ],
-        items2: [
-          {
-            name:'Вахтовый',
-            descr_name:'(12 ч.)',
-            price_hour:140,
-            price:80000,
-            price_hour_with_weapon:160,
-            price_with_weapon:85000,
-          },
-          {
-            name:'Суточный',
-            descr_name:'(2/2)',
-            price_hour:165,
-            price:105000,
-            price_hour_with_weapon:185,
-            price_with_weapon:110000,
-          },
-          {
-            name:'Суточный',
-            descr_name:'(1/2)',
-            price_hour:170,
-            price:125000,
-            price_hour_with_weapon:190,
-            price_with_weapon:130000,
-          },
-          {
-            name:'Дневной (ночной)',
-            descr_name:'(12 часов, пн-вс)',
-            price_hour:145,
-            price:85000,
-            price_hour_with_weapon:165,
-            price_with_weapon:90000,
-          },
-          {
-            name:'Дневной (ночной)',
-            descr_name:'(12 часов, пн-пт)',
-            price_hour:140,
-            price:75000,
-            price_hour_with_weapon:160,
-            price_with_weapon:80000,
-          },
-        ],
-        items3: [
-          {
-            name:'Вахтовый',
-            descr_name:'(12 ч.)',
-            price_hour:145,
-            price:85000,
-            price_hour_with_weapon:165,
-            price_with_weapon:90000,
-          },
-          {
-            name:'Суточный',
-            descr_name:'(2/2)',
-            price_hour:175,
-            price:120000,
-            price_hour_with_weapon:195,
-            price_with_weapon:125000,
-          },
-          {
-            name:'Суточный',
-            descr_name:'(1/2)',
-            price_hour:185,
-            price:135000,
-            price_hour_with_weapon:200,
-            price_with_weapon:140000,
-          },
-          {
-            name:'Дневной (ночной)',
-            descr_name:'(12 ч., пн-вс)',
-            price_hour:165,
-            price:100000,
-            price_hour_with_weapon:185,
-            price_with_weapon:105000,
-          },
-          {
-            name:'Дневной (ночной)',
-            descr_name:'(12 часов, пн-пт)',
-            price_hour:145,
-            price:85000,
-            price_hour_with_weapon:165,
-            price_with_weapon:90000,
-          },
-        ]
       }
     }
   }
