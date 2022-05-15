1c60a52c29d4ee098b46f9699b208f4127955b73
AnigalHub <anigal1997@gmail.com>
2022-05-16 01:12:05 +0300

    добавлен <br>

--- components/LandingPage/import_numbers.vue
+++ components/LandingPage/import_numbers.vue
@@ -18,7 +18,7 @@
         return{
           important_numbers:[
             {number:15, descr_number: "Лет на рынке <br> безопасности",},
-            {number:564, descr_number: "Профессиональных сотрудников",},
+            {number:564, descr_number: "Профессиональных <br> сотрудников",},
             {number:50, descr_number: "Благодарностей <br> и рекомендаций",},
             {number:74, descr_number: "Постоянных <br> клиентов",},
           ],
