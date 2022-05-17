af56f774a2dd8308b3d57458e2e4e8b72fb4a09d
AnigalHub <anigal1997@gmail.com>
2022-05-17 23:19:59 +0300

    удалены отступы

--- components/LandingPage/import_numbers.vue
+++ components/LandingPage/import_numbers.vue
@@ -17,22 +17,10 @@
       data(){
           return{
             important_numbers:[
-              {
-                number:15,
-                descr_number: "Лет на рынке <br> безопасности",
-              },
-              {
-                number:564,
-                descr_number: "Профессиональных сотрудников",
-              },
-              {
-                number:50,
-                descr_number: "Благодарностей <br> и рекомендаций",
-              },
-              {
-                number:74,
-                descr_number: "Постоянных <br> клиентов",
-              },
+              {number:15, descr_number: "Лет на рынке <br> безопасности",},
+              {number:564, descr_number: "Профессиональных сотрудников",},
+              {number:50, descr_number: "Благодарностей <br> и рекомендаций",},
+              {number:74, descr_number: "Постоянных <br> клиентов",},
             ],
           }
       }
